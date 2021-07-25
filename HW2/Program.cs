b431bf656e8d748ea61a0a86c02b656656a25340
Dimi4ik44 <Dimi4ik44@gmail.com>
2021-07-25 14:18:18 +0300

    task1 compleated

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -6,7 +6,33 @@ namespace HW2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number, result;
+            int maxNumbers = 5;
+            string errorMessage = String.Empty;
+
+            Console.WriteLine("Введите 5-ти значное число");
+            bool succesNumber = Int32.TryParse(Console.ReadLine(),out number);
+            if(!succesNumber)
+            {
+                errorMessage += "Wrong type input";
+            }
+            else
+            {
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    double n = (int)(number / Math.Pow(10,i)) % 10;
+                //    //number = number / 10;
+                //    Console.WriteLine((i+1) + " цифра " + n);
+                //}
+                for (int i = 0; i < maxNumbers; i++)
+                {
+                    double n = (int)(number / Math.Pow(10, (maxNumbers-1)-i)) % 10;
+                    Console.WriteLine((i + 1) + " цифра " + n);
+                }
+            }
+
+
+            
         }
     }
 }
