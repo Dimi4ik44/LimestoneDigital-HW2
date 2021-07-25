80fe310223da758adc966f7e3dc4062d3de3a731
Dimi4ik44 <Dimi4ik44@gmail.com>
2021-07-25 17:59:32 +0300

    Calculator: +handing double +change error masages

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -7,14 +7,14 @@ namespace HW2
         static void Main(string[] args)
         {
             //1,2
-            int number, result;
+            int number;
             int maxNumbers = 5;
             int max = 0, min = 10;
             string errorMessage = String.Empty;
 
             Console.WriteLine("Введите " + maxNumbers + "-ти значное число");
-            bool succesNumber = Int32.TryParse(Console.ReadLine(),out number);
-            if(!succesNumber)
+            bool succesNumber = Int32.TryParse(Console.ReadLine(), out number);
+            if (!succesNumber)
             {
                 errorMessage += "Wrong type input";
             }
@@ -28,30 +28,106 @@ namespace HW2
                 //}
                 for (int i = 0; i < maxNumbers; i++)
                 {
-                    int curDigit = (int)(number / Math.Pow(10, (maxNumbers-1)-i)) % 10;
-                    if(max < curDigit)
+                    int curDigit = (int)(number / Math.Pow(10, (maxNumbers - 1) - i)) % 10;
+                    if (max < curDigit)
                     {
                         max = curDigit;
                     }
-                    if(min > curDigit)
+                    if (min > curDigit)
                     {
                         min = curDigit;
                     }
                     Console.WriteLine((i + 1) + " цифра " + curDigit);
                 }
                 Console.WriteLine("maxDigit: " + max + " minDigit: " + min);
+               
             }
-            if(errorMessage==String.Empty)
+
+            if (errorMessage.Length > 0)
             {
-                Console.WriteLine("Ошибки: All done");
+                Console.WriteLine(errorMessage);
+                errorMessage = String.Empty;
+            }
+
+            //Calculator-----------------------------------------------------
+            Console.WriteLine("Calculator");
+            bool wrongInput = true;
+            double number1 = 0, number2 = 0;
+            double result = 0;
+
+
+            while (wrongInput)
+            {
+                Console.Write("First: ");
+                bool s1 = double.TryParse(Console.ReadLine(), out number1);
+
+                Console.Write("Second: ");
+                bool s2 = double.TryParse(Console.ReadLine(), out number2);
+                wrongInput = !(s2 && s1);
+
+                if (wrongInput)
+                {
+                    errorMessage += "Incorrect calculator input; ";
+                    Console.WriteLine(errorMessage);
+                    errorMessage = String.Empty;
+                }
+            }
+
+            Console.Write("Operation: ");
+
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "*":
+                    result += number1 * number2;
+                    break;
+                case "+":
+                    result += number1 + number2;
+                    break;
+                case "-":
+                    result += number1 - number2;
+                    break;
+                case "/" when number2 != 0:
+                    result += number1 / number2;
+                    break;
+                default:
+                    errorMessage += "Incorrect calculator operation; ";
+                    break;
+            }
+
+            if (errorMessage.Length > 0)
+            {
+                Console.WriteLine(result);
+                Console.WriteLine(errorMessage);
+                errorMessage = String.Empty;
+            }
+            else
+            {
+                Console.WriteLine("Result: " + result);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (errorMessage == String.Empty)
+            {
+                Console.WriteLine("All done");
             }
             else
             {
                 Console.WriteLine("Ошибки: " + errorMessage);
             }
 
-
-            
         }
     }
 }
